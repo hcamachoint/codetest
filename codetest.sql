f16d0091c9f850c6c15e0adc41a72d855fa1e482
dunicode <dunicode@TUX.localdomain>
2020-05-03 12:35:30 -0400

    ready

--- codetest.sql
+++ codetest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 02-05-2020 a las 01:07:24
+-- Tiempo de generación: 03-05-2020 a las 13:33:50
 -- Versión del servidor: 10.3.22-MariaDB-0+deb10u1
 -- Versión de PHP: 7.3.14-1~deb10u1
 
@@ -32,16 +32,11 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `uuid` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
   `password` varchar(200) NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`id`, `uuid`, `email`, `password`, `status`) VALUES
-(1, '87f2ac07-37ff-4338-aedd-e8325a0d53d3', 'admin@local.dev', 'debc659d41b143c4b9dabd5471994dc22431857b96cb2c870faa6ebd2e1cc881c9645c587941499adc33fef94bc035db626e8d9edeb72cf9367ed840e0b421503kkKCes0rO+QwbswPCJE/b2hJtjHKcXvTX5hkNMp+Qc=', 1);
 
 --
 -- Índices para tablas volcadas
@@ -51,7 +46,9 @@ INSERT INTO `users` (`id`, `uuid`, `email`, `password`, `status`) VALUES
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uuid` (`uuid`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -61,7 +58,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
